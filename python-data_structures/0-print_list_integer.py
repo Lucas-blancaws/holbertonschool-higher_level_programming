5c680a82ef7c8e74067203b83fa4cf870050dc48
Lucas-blancaws <lblancportier@gmail.com>
2025-09-09 10:12:49 +0200

    fix

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,15 +1,7 @@
-def print_list_integer(my_list=None):
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
     '''
-    Function:
-        print_list_integer(my_list=None): prints all integers of a list
-
-    Parameters:
-        my_list: list of integers (default: None)
-
-    Returns:
-        None
+    Prints all integers of a list.
     '''
-    if my_list is None:
-        my_list = []
     for number in my_list:
         print("{:d}".format(number))
