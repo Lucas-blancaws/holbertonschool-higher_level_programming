83941e9d33693e736e98018fd862de63e49e3f7e
Lucas-blancaws <lblancportier@gmail.com>
2025-09-10 12:29:43 +0200

    no commentary in code

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    '''
-    Returns a new matrix with the square of all values of the input matrix.
-    '''
     new_matrix = []
     for row in matrix:
         new_row = []
