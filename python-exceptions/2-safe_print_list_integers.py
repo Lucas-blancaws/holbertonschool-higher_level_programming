1bfe918fe8a1312f1b719f1e0614bbb98cb83a8e
Lucas-blancaws <lblancportier@gmail.com>
2025-09-12 11:47:47 +0200

    fix

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,12 +3,11 @@ def safe_print_list_integers(my_list=[], x=0):
     compteur = 0
     for i in range(x):
         try:
-            value = my_list[i]
             print("{:d}".format(my_list[i]), end="")
             compteur += 1
-        except (TypeError, ValueError):
-            continue
         except IndexError:
             break
+        except (TypeError, ValueError):
+            pass
     print()
     return compteur
