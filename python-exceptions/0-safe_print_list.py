aa4ee0b4916481c54821e120a40b4e7294ff6f7f
Lucas-blancaws <lblancportier@gmail.com>
2025-09-12 11:33:24 +0200

    fix

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     compteur = 0
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             print(my_list[i], end="")
             compteur += 1
-    except IndexError:
-        print("depassement taille de liste")
+        except IndexError:
+            break
+        finally:
+             pass
     print()
     return compteur
