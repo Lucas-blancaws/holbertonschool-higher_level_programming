fe58a428442c314cc8bab52d795c318ca9069adc
Lucas-blancaws <lblancportier@gmail.com>
2025-10-09 12:48:41 +0200

    task3

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,40 @@
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
+    print(f"Status Code: {response.status_code}")
+
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post["title"])
+    else:
+        print("Erreur lors de la récupération des posts.")
+
+
+def fetch_and_save_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        posts = response.json()
+
+        posts_data = [
+            {"id": post["id"], "title": post["title"], "body": post["body"]}
+            for post in posts
+        ]
+
+        with open("posts.csv", "w", newline="", encoding="utf-8") as csvfile:
+            fieldnames = ["id", "title", "body"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerows(posts_data)
+
+        print("Les posts ont été enregistrés dans posts.csv")
+    else:
+        print("Erreur lors de la récupération des posts.")
