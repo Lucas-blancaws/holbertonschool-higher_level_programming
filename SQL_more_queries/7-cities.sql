85a20afe3b76b02dd7ee28b2d2c9649c720f72e8
Lucas-blancaws <lblancportier@gmail.com>
2025-10-23 01:33:40 +0200

    1/7

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- Create table cities with foreign key to states
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    state_id INT NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id),
+    name VARCHAR(256) NOT NULL
+    );
