ed034773aa149ae619e6507333a1a8f5424cc7e2
Lucas-blancaws <lblancportier@gmail.com>
2025-10-23 01:36:33 +0200

    coorection 1

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,3 @@
--- creates a database
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+-- Create MySQL user with all privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
