3aa715481524c73255e57cfd4ca72730f4a95dfb
Lucas-blancaws <lblancportier@gmail.com>
2025-10-23 09:09:49 +0200

    8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 --loool
 SELECT id, name FROM cities
 WHERE state_id = (
-    SELECT id FROM states WHERE name = "California"
+    SELECT id FROM states WHERE name = 'California'
 )
 ORDER BY id ASC;
