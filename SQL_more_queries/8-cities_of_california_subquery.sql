70b089486a4005a796038fa31cc278f214956859
Lucas-blancaws <lblancportier@gmail.com>
2025-10-23 08:56:05 +0200

    test

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 --aa
 SELECT id, name FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+WHERE state_id = (SELECT id FROM states WHERE name = "California")
 ORDER BY id;
