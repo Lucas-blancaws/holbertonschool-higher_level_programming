74ec8e1e65773263157cb538e8f8079d5ccae776
Lucas-blancaws <lblancportier@gmail.com>
2025-11-06 14:51:49 +0100

    2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-if (process.argv.length <= 2) {
+if (args.length === 0) {
   console.log('No argument');
-} else if (process.argv.length <= 3) {
+} else if (args.length === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
