b9fa4394f3e1969f07b75bc9523365efb4a1d03b
Lucas-blancaws <lblancportier@gmail.com>
2025-11-06 15:29:37 +0100

    3

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
+const arg = process.argv[2];
 
-if (args.length === 0) {
+if (arg === undefined) {
   console.log('No argument');
 } else {
-  console.log('Arguments found');
+  console.log(arg);
 }
